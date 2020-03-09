76197fa54fc8aab39c7c3cc628e1b077dbaaabf9
Aluno <rafael.zottesso@ifpr.edu.br>
2020-03-09 09:30:41 -0300

    CSS: bordas, margem e padding.

---
+++ aula04.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 04 - CSS e HTML</title>
+    <!-- Primeiro importe as fontes -->
+    <link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave|Roboto&display=swap" rel="stylesheet">
+    <!-- Toda importação de CSS deve ser feita no HEAD -->
+    <link rel="stylesheet" href="aula04.css">
+  </head>
+  <body>
+
+    <div class="site">
+
+      <div class="topo">
+        <h2>Bagaçamos no CSS</h2>
+      </div>
+
+      <div class="conteudo">
+        Corpo do site
+      </div>
+
+      <div class="rodape">
+        Desenvolvido por Rafael Zottesso
+      </div>
+
+    </div>
+
+  </body>
+</html>

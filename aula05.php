360873bee0f1985fa5b9b5a6fe65254372deeafa
Aluno <rafael.zottesso@ifpr.edu.br>
2020-03-16 09:39:57 -0300

    Criação de botões e de um menu horizontal.

---
+++ aula05.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 04 - CSS e HTML</title>
+    <!-- Primeiro importe as fontes -->
+    <link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave|Roboto&display=swap" rel="stylesheet">
+    <!-- Toda importação de CSS deve ser feita no HEAD -->
+    <link rel="stylesheet" href="aula05.css">
+  </head>
+  <body>
+
+    <div class="site">
+
+      <div class="topo">
+        <h2>Criação de menu e botões</h2>
+
+        <div class="menu">          
+          <ul>
+            <li><a href="#">Página inicial</a></li>
+            <li><a href="#">Cadastro</a></li>
+            <li><a href="#">Contato</a></li>
+            <li><a href="#">Ajuda</a></li>
+            <li><a href="#">Login</a></li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div class="conteudo">
+
+        <h3>Criação de Botões</h3>
+
+        <a href="#" class="botao botao-azul">Botão Azul</a>
+        <a href="#" class="botao botao-verde">Botão Verde</a>
+        <a href="#" class="botao botao-vermelho">Botão Vermelho</a>
+
+      </div>
+
+      <div class="rodape">
+        Desenvolvido por Rafael Zottesso
+      </div>
+
+    </div>
+
+  </body>
+</html>
